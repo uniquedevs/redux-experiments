d8adfe5060667a0023d786c47be04181278e9b29
max <maxim.evdokimov@loudclear.com.au>
2018-06-12 09:09:39 +0300

    endpioints

--- src/endpoints/api.js
+++ src/endpoints/api.js
@@ -1,12 +1,19 @@
-export const api = () => ({
-    get: (url) => {
-        return window.fetch(url, {
-            method: 'get',
-            headers: {
-                Authorization: 'Bearer 33f1e0c38454747d065499a9fa5c867718548f65'
-            }
-        })
-            .then( resp => resp.json())
-            .then( resp => resp.data )
-    }
-});
+export const api = () => {
+	const headers = {
+		Authorization: 'Bearer 33f1e0c38454747d065499a9fa5c867718548f65'
+	};
+	const api = Object.keys(methods).reduce(
+		(api, key) => (url, params) => window.fetch(url, {  })
+	)
+};
+
+const methods = {
+	get: {
+			headers: {
+				Authorization: 'Bearer 33f1e0c38454747d065499a9fa5c867718548f65'
+			}
+		})
+			.then(resp => resp.json())
+			.then(resp => resp.data)
+	}
+};
